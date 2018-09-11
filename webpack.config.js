efb995e0456ba69a57655e98ca35e1b756335632
NghiaNT <n.nghia@air-closet.com>
2018-09-11 17:43:55 +0900

    finish basic webpack in react project

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,41 @@
 const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const VENDOR_LIBS = [
+	'axios',
+	'react',
+	'react-dom',
+	'redux',
+	'redux-thunk'
+];
+const devServer = {
+	port: 3000,
+	open: true,
+	disableHostCheck: true,
+	historyApiFallback: true,
+	overlay: true,
+	stats: 'minimal',
+	inline: true,
+	compress: true,
+	contentBase: '/'
+};
 
 module.exports = {
 	entry: {
-		bundle: './src/index.js'
+		bundle: './src/index.js',
+		vendor: VENDOR_LIBS,
 	},
 	output: {
 		path: path.join(__dirname, 'dist'),
-		filename: 'bundle.js'
+		filename: '[name].[chunkhash].js'
 	},
 	module: {
 		rules: [
 			{
 				use: 'babel-loader',
 				test: /\.js$/,
-				exclude: '/node-modules'
+				exclude: '/node_modules'
 			},
 			{
 				use: [
@@ -23,5 +45,23 @@ module.exports = {
 				test: /\.css$/,
 			}
 		]
-	}
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: 'src/index.html'
+		})
+	],
+	optimization: {
+    runtimeChunk: "single", // enable "runtime" chunk
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendor",
+          chunks: "all"
+        }
+      }
+    }
+	},
+	devServer
 }
